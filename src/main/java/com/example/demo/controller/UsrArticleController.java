0b7d424e974ee2f1e683a7c927ab216706f24ca7
meustar <meustar829@gmail.com>
2024-08-09 15:31:21 +0900

    service 구조 추가, 기능 이전

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -1,37 +1,60 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.service.ArticleService;
 import com.example.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
 
-	int lastArticleId;
-
-	List<Article> articles;
-
-	// 생성자
-	public UsrArticleController() {
-		lastArticleId = 0;
-		articles = new ArrayList<>();
-
-		makeTestData();
-	}
+	// 서비스의 생성자가 없는데도 사용할수 있다.
+	@Autowired
+	private ArticleService articleService;
 
 	// 액션 메서드, 컨트롤 메서드
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticle(int id) {
+
+		Article article = articleService.getArticleById(id);
+
+		if (article == null) {
+			return id + "번 글은 없습니다.";
+		}
+
+		return article;
+	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public Object doModify(int id, String title, String body) {
+
+		Article article = articleService.getArticleById(id);
+
+		if (article == null) {
+			return id + "번 글은 없습니다.";
+		}
+
+		// getter, setter 잘 알아보자... ㅠㅠ
+//		article.setTitle(title);
+//		article.setBody(body);
+		articleService.modifyArticle(id, title, body);
+
+		return article;
+	}
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
 
 		// id가 있는지부터 알아야 함.
-		Article article = getArticleById(id);
+		Article article = articleService.getArticleById(id);
 
 		if (article == null) {
 			return id + "번 글은 없습니다.";
@@ -39,56 +62,17 @@ public class UsrArticleController {
 
 		// (id - 1)로 인덱스를 사용해서 지우게 되면. 중간값의 인덱스를 지우면
 		// 그 뒷 열의 모든 인덱스가 망가져서 엉뚱한 글을 지우게 된다.
-		articles.remove(article);
+//		articles.remove(article);
+		articleService.deleteArticle(id);
 
 		return id + "번 글이 삭제되었습니다.";
 	}
-
-	private Article getArticleById(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-
-		return null;
-	}
-
-	@RequestMapping("/usr/article/doModify")
-	@ResponseBody
-	public Object doModify(int id, String title, String body) {
-	
-		Article article = getArticleById(id);
-
-		if (article == null) {
-			return id + "번 글은 없습니다.";
-		}
-		
-		// getter, setter 잘 알아보자... ㅠㅠ
-		article.setTitle(title);
-		article.setBody(body);
-		
-		return article;
-	}
-
 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
 
-		Article article = writeArticle(title, body);
-		return article;
-	}
-	@RequestMapping("/usr/article/getArticle")
-	@ResponseBody
-	public Object getArticle(int id) {
-		
-		Article article = getArticleById(id);
-
-		if (article == null) {
-			return id + "번 글은 없습니다.";
-		}
-		
+		Article article = articleService.writeArticle(title, body);
 		return article;
 	}
 
@@ -96,33 +80,7 @@ public class UsrArticleController {
 	@ResponseBody
 	public List<Article> getArticles() {
 
-		return articles;
-	}
-
-	// 서비스 메서드
-
-	public void makeTestData() {
-
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목" + i;
-			String body = "내용" + i;
-
-			writeArticle(title, body);
-		}
-	}
-
-	private Article writeArticle(String title, String body) {
-
-		int id = lastArticleId + 1;
-
-		Article article = new Article(id, title, body);
-
-		articles.add(article);
-
-		lastArticleId++;
-
-		return article;
-
+		return articleService.articles;
 	}
 
 }
