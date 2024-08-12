f42d25dd0383e27015bbdb12e4a7418bd9fda671
meustar <meustar829@gmail.com>
2024-08-12 11:39:02 +0900

    DB스키마 업데이트

--- DB.sql
+++ DB.sql
@@ -8,17 +8,19 @@ CREATE TABLE article(
       title CHAR(100) NOT NULL,
       `body` TEXT NOT NULL
 );
-
 CREATE TABLE `member`(
       id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
       regDate DATETIME NOT NULL,
       updateDate DATETIME NOT NULL,
       loginId CHAR(30) NOT NULL,
       loginPw CHAR(100) NOT NULL,
-      `name` CHAR(20) NOT NULL,
-      nickname CHAR(20) NOT NULL,
-      cellphoneNum CHAR(20) NOT NULL,
-      email CHAR(50) NOT NULL
+      `authLevel` smallint(2) unsigned default 3 comment '권한 레벨 (3=일반,7=관리자)',
+      `name` char(20) not null,
+      nickname char(20) not null,
+      cellphoneNum char(20) not null,
+      email char(50) not null,
+      delStatus tinyint(1) unsigned not null default 0 comment '탈퇴 여부 (0=탈퇴 전, 1=탈퇴 후)',
+      delDate datetime comment '탈퇴 날짜'
 );
 
 ## 게시글 테스트 데이터 생성
@@ -40,16 +42,19 @@ title = '제목3',
 
 
 ## 회원 테스트 데이터 생성
+## (관리자)
 INSERT INTO `member`
 SET regDate = NOW(),
 updateDate = NOW(),
 loginId = 'admin',
 loginPw = 'admin',
+`authLevel` = 7,
 `name` = '관리자',
 nickname = '관리자',
 cellphoneNum = '01012341234',
 email = 'abc@gmail.com';
 
+## (일반)
 INSERT INTO `member`
 SET regDate = NOW(),
 updateDate = NOW(),
@@ -60,6 +65,7 @@ nickname = '회원1_닉네임',
 cellphoneNum = '01043214321',
 email = 'abcd@gmail.com';
 
+## (일반)
 INSERT INTO `member`
 SET regDate = NOW(),
 updateDate = NOW(),
@@ -69,13 +75,15 @@ loginPw = 'test2',
 nickname = '회원2_닉네임',
 cellphoneNum = '01056785678',
 email = 'abcde@gmail.com';
-
 SELECT *
 FROM article
 ORDER BY id DESC;
-
 SELECT *
 FROM `member`;
-
-
 ###############################################################################
+## 게시글 테스트 데이터 대량 생성
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = CONCAT('제목__', RAND()),
+`body` = CONCAT('내용__', RAND());
