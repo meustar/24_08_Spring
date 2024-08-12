19e456ea67ac8bc86167b23f84c8ef2c3c5c1cd9
meustar <meustar829@gmail.com>
2024-08-12 11:35:36 +0900

    회원가입 기능 추가, 회원 테이블 및 테스트데이터 추가

--- src/main/java/com/example/demo/vo/Member.java
+++ src/main/java/com/example/demo/vo/Member.java
@@ -1,15 +1,21 @@
 package com.example.demo.vo;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Article {
+public class Member {
 
 	private int id;
 	private String regDate;
 	private String updateDate;
-	private String title;
-	private String body;
+	private String loginId;
+	private String name;
+	private String nickname;
+	private String cellphoneNum;
+	private String email;
+
 }
