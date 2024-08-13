fc8031137f3c0bb04f452f216e0df7516c0ede3f
meustar <meustar829@gmail.com>
2024-08-13 15:14:20 +0900

    게시글 목록 구현, jstl 설정

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -9,5 +9,18 @@ import com.example.demo.vo.Article;
 @Controller
 public class UsrHomeController {
 	
+	// @ResponseBody 가 있으면 return 값을 화면에 보여주고(직접 응답), 없으면 경로 설정.
+
+//	@ResponseBody
+	@RequestMapping("/usr/home/main")
+	public String showMain() {
+		return "/usr/home/main";
+	}
 	
+	
+//	@ResponseBody
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
+	}
 }
