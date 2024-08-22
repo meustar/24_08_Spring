9ae061b0ef9463fa01f8d4eb3f8131932bf09aee
meustar <meustar829@gmail.com>
2024-08-22 14:30:25 +0900

    DB업데이트, 좋아요 테이블 추가

--- DB.sql
+++ DB.sql
@@ -18,14 +18,16 @@ CREATE TABLE `member`(
       updateDate DATETIME NOT NULL,
       loginId CHAR(30) NOT NULL,
       loginPw CHAR(100) NOT NULL,
-      `authLevel` smallint(2) unsigned default 3 comment '권한 레벨 (3=일반,7=관리자)',
-      `name` char(20) not null,
-      nickname char(20) not null,
-      cellphoneNum char(20) not null,
-      email char(50) not null,
-      delStatus tinyint(1) unsigned not null default 0 comment '탈퇴 여부 (0=탈퇴 전, 1=탈퇴 후)',
-      delDate datetime comment '탈퇴 날짜'
+      `authLevel` SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '권한 레벨 (3=일반,7=관리자)',
+      `name` CHAR(20) NOT NULL,
+      nickname CHAR(20) NOT NULL,
+      cellphoneNum CHAR(20) NOT NULL,
+      email CHAR(50) NOT NULL,
+      delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴 여부 (0=탈퇴 전, 1=탈퇴 후)',
+      delDate DATETIME COMMENT '탈퇴 날짜'
 );
+
+
 ## 게시글 테스트 데이터 생성
 INSERT INTO article
 SET regDate = NOW(),
@@ -79,13 +81,18 @@ loginPw = 'test2',
 nickname = '회원2_닉네임',
 cellphoneNum = '01056785678',
 email = 'abcde@gmail.com';
-alter table article add column memberId int(10) unsigned not null after updateDate;
-update article
-set memberId = 2
-where id in (1,2);
-update article
-set memberId = 3
-where id in (3,4);
+
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
+
+UPDATE article
+SET memberId = 2
+WHERE id IN (1,2);
+
+UPDATE article
+SET memberId = 3
+WHERE id IN (3,4);
+
+
 # 게시판(board) 테이블 생성
 CREATE TABLE board (
       id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -112,7 +119,9 @@ SET regDate = NOW(),
 updateDate = NOW(),
 `code` = 'QnA',
 `name` = '질의응답';
-alter table article add column boardId int(10) unsigned not null after `memberId`;
+
+ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER `memberId`;
+
 UPDATE article
 SET boardId = 1
 WHERE id IN (1,2);
@@ -123,53 +132,117 @@ UPDATE article
 SET boardId = 3
 WHERE id = 4;
 
-alter table article add column hitCount int(10) unsigned not null default 0 after `body`;
+ALTER TABLE article ADD COLUMN hitCount INT(10) UNSIGNED NOT NULL DEFAULT 0 AFTER `body`;
+
+
+
+# reactionPoint 테이블 생성
+CREATE TABLE reactionPoint(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    memberId INT(10) UNSIGNED NOT NULL,
+    relTypeCode CHAR(50) NOT NULL COMMENT '관련 데이터 타입 코드',
+    relId INT(10) NOT NULL COMMENT '관련 데이터 번호',
+    `point` INT(10) NOT NULL
+);
+
+# reactionPoint 테스트 데이터 생성
+# 1번 회원이 1번 글에 싫어요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 1,
+relTypeCode = 'article',
+relId = 1,
+`point` = -1;
+
+# 1번 회원이 2번 글에 좋아요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 1,
+relTypeCode = 'article',
+relId = 2,
+`point` = 1;
+
+# 2번 회원이 1번 글에 싫어요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 2,
+relTypeCode = 'article',
+relId = 1,
+`point` = -1;
+
+# 2번 회원이 2번 글에 싫어요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 2,
+relTypeCode = 'article',
+relId = 2,
+`point` = -1;
+
+# 3번 회원이 1번 글에 좋아요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 3,
+relTypeCode = 'article',
+relId = 1,
+`point` = 1;
 
 ###(INIT 끝)
 ##########################################
 SELECT *
 FROM article
 ORDER BY id DESC;
-SELECT *
-FROM board;
-SELECT *
-FROM `member`;
+
+SELECT * FROM board;
+
+SELECT * FROM `member`;
+
+SELECT * FROM `reactionPoint`;
+
 ###############################################################################
+
 ## 게시글 테스트 데이터 대량 생성
 INSERT INTO article
 (
     regDate, updateDate, memberId, boardId, title, `body`
 )
-select now(), now(), floor(RAND() * 2) + 2, FLOOR(RAND() * 3) + 1, CONCAT('제목__', RAND()), CONCAT('내용__', RAND())
-from article;
-select floor(RAND() * 2) + 2
+SELECT NOW(), NOW(), FLOOR(RAND() * 2) + 2, FLOOR(RAND() * 3) + 1, CONCAT('제목__', RAND()), CONCAT('내용__', RAND())
+FROM article;
+
+
+SELECT FLOOR(RAND() * 2) + 2
+
 SELECT FLOOR(RAND() * 3) + 1
+
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = CONCAT('제목__', RAND()),
 `body` = CONCAT('내용__', RAND());
 
-show full columns from `member`;
-desc `member`;
+SHOW FULL COLUMNS FROM `member`;
+DESC `member`;
 
 SELECT *
 FROM article
-where boardId = 1
+WHERE boardId = 1
 ORDER BY id DESC;
 
 SELECT *
 FROM article
 WHERE boardId = 2
 ORDER BY id DESC;
-
 SELECT *
 FROM article
 WHERE boardId = 3
 ORDER BY id DESC;
-
 '111'
-
 SELECT COUNT(*) AS cnt
 FROM article
 WHERE boardId = 1
@@ -177,27 +250,29 @@ ORDER BY id DESC;
 
 SELECT *
 FROM article
-WHERE boardId = 1 and title like '%123%'
+WHERE boardId = 1 AND title LIKE '%123%'
 ORDER BY id DESC;
 
 SELECT *
 FROM article
-WHERE boardId = 1 and `body` like '%123%'
+WHERE boardId = 1 AND `body` LIKE '%123%'
 ORDER BY id DESC;
 
 SELECT *
 FROM article
-WHERE boardId = 1 and title like '%123%' or `body` like '%123%'
+WHERE boardId = 1 AND title LIKE '%123%' OR `body` LIKE '%123%'
 ORDER BY id DESC;
 
-SELECT count(*)
+SELECT COUNT(*)
 FROM article AS A
 WHERE A.boardId = 1 
 ORDER BY A.id DESC;
-
 boardId=1&searchKeywordTypeCode=nickname&searchKeyword=1
 
 SELECT COUNT(*)
 FROM article AS A
-WHERE A.boardId = 1 and A.memberId = 3
+WHERE A.boardId = 1 AND A.memberId = 3
 ORDER BY A.id DESC;
+
+SELECT hitCount
+FROM article WHERE id = 3
