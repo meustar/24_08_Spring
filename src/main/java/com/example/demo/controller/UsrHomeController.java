b1b72230abf2127197a5166b4154b742cc9e6de6
meustar <meustar829@gmail.com>
2024-09-02 16:12:18 +0900

    melon Top100 크롤링(Selenium) 테스트

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.util.crawlTest;
+
 @Controller
 public class UsrHomeController {
 	
@@ -20,4 +22,13 @@ public class UsrHomeController {
 	public String showRoot() {
 		return "redirect:/usr/home/main";
 	}
+	
+	@RequestMapping("/usr/crawl")
+	public String doCrawl() {
+
+		crawlTest.crawl();
+
+		return "redirect:/usr/home/main";
+	}
+	
 }
